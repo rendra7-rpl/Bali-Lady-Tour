e76c993f8e0111a7ae1e4b77426c1a395f35b3a0
IMadeSatriaWintara <satriawintaraimade@gmail.com>
2025-09-15 09:53:22 +0800

    tambah program

--- database/migrations/2025_09_15_011540_create_programs_table.php
+++ database/migrations/2025_09_15_011540_create_programs_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('programs', function (Blueprint $table) {
             $table->id();
+            $table->string('foto');
+            $table->text('title');
+            $table->longtext('content');
             $table->timestamps();
         });
     }
