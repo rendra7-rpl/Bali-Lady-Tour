a49ec2680800cb7516de7d375f36a70e4de99b9a
IMadeSatriaWintara <satriawintaraimade@gmail.com>
2025-09-15 10:55:38 +0800

    upadte kode

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GaleriController;
+use App\Http\Controllers\BeritaController;
+use App\Http\Controllers\ProgramController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Backend.Dashboard.dashboard');
 });
 
+Route::get('/dashboard', [BeritaController::class, 'dashboard'])->name('dashboard');
+
+// ## GALERY ## //
+Route::get('/galeri', [GaleriController::class, 'galeri'])->name('galeri');
+Route::get('/tambahgaleri',[GaleriController::class, 'tambahgaleri'])->name('tambahgaleri');
+Route::post('/insertgaleri',[GaleriController::class, 'insertgaleri'])->name('insertgaleri');
+Route::get('/tampilkangaleri/{id}',[GaleriController::class, 'tampilkangaleri'])->name('tampilkangaleri');
+Route::post('/updategaleri/{id}',[GaleriController::class, 'updategaleri'])->name('updategaleri');
+Route::get('/deletegaleri/{id}',[GaleriController::class, 'deletegaleri'])->name('deletegaleri');
+
+// ## PROGRAM ## //
+Route::get('/programs',[ProgramController::class, 'programs'])->name('programs');
+Route::get('/tambahprogram',[ProgramController::class, 'tambahprogram'])->name('tambahprogram');
+Route::post('/insertprogram',[ProgramController::class, 'insertprogram'])->name('insertprogram');
+Route::get('/tampilkanprogram/{id}',[ProgramController::class, 'tampilkanprogram'])->name('tampilkanprogram');
+Route::post('/updateprogram/{id}',[ProgramController::class, 'updateprogram'])->name('updateprogram');
+Route::get('/deleteprogram/{id}',[ProgramController::class, 'deleteprogram'])->name('deleteprogram');
+
+// ## BERITA  ## //
+Route::get('/newss',[BeritaController::class, 'newss'])->name('newss');
+Route::get('/tambahdata',[BeritaController::class, 'tambahdata'])->name('tambahdata');
+Route::post('/insertdata',[BeritaController::class, 'insertdata'])->name('insertdata');
+Route::get('/tampilkandata/{id}',[BeritaController::class, 'tampilkandata'])->name('tampilkandata');
+Route::post('/updatedata/{id}',[BeritaController::class, 'updatedata'])->name('updatedata');
+Route::get('/deletenews/{id}',[BeritaController::class, 'deletenews'])->name('deletenews');
+Route::get('/exportpdf',[BeritaController::class, 'exportpdf'])->name('exportpdf');
