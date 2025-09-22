425a6f5f18dc8798492aee660e04e93a098d769d
IMadeSatriaWintara <satriawintaraimade@gmail.com>
2025-09-22 10:57:08 +0800

    update

--- resources/views/Backend/Layout/admin.blade.php
+++ resources/views/Backend/Layout/admin.blade.php
@@ -81,7 +81,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="" class="brand-link">
+    <a href="/" class="brand-link">
       <img src="{{ asset('aset/logo.png') }}" alt="OLga Sehat Logo" class="brand-image img-circle elevation-3" style="opacity: .5">
       <span class="brand-text font-weight-light">Bali Lady Tour</span>
     </a>
